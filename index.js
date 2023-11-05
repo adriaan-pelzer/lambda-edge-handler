9066c77bab3a426d050fb32612e2312385d17381
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2023-11-05 01:22:44 +0000

    fix naming problems

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const querystring = require('node:querystring');
 
 const getRequest = event => event.Records[0].cf.request;
-const getMethod = event => get_request(event).method;
-const getQuery = event => querystring.parse(get_request(event).querystring);
-const getCustomHeader = event => header => get_request(event).origin?.s3?.customHeaders[header][0].value;
+const getMethod = event => getRequest(event).method;
+const getQuery = event => querystring.parse(getRequest(event).querystring);
+const getCustomHeader = event => header => getRequest(event).origin?.s3?.customHeaders[header][0].value;
 
 module.exports = ({ allowedMethods, event }) => new Promise(
   (resolve, reject) => allowedMethods.indexOf(getMethod(event)) === -1 ?
-    reject({ statusCode: 405, error: `` }) :
-    resolve({ request: getRequest(event), query: getQuery(event), getCustomHeader: getCustomHeader(event) })
+    reject({
+      statusCode: 405,
+      error: `Only ${allowedMethods.map(method => `"${method}"`).join(', ')} method${allowedMethods.length === 1 ? '' : 's'} allowed`
+    }) :
+    resolve({
+      request: getRequest(event),
+      query: getQuery(event),
+      getCustomHeader: getCustomHeader(event)
+    })
 );
