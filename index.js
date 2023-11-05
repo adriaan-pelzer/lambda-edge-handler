bfc28cb2bd2a90cc660d43b3f8535e69cfd7578b
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2023-11-05 11:23:12 +0000

    Handle HEAD and OPTIONS body-less responses, and handle body checking

--- index.js
+++ index.js
@@ -5,15 +5,39 @@ const getMethod = event => getRequest(event).method;
 const getQuery = event => querystring.parse(getRequest(event).querystring);
 const getCustomHeader = event => header => getRequest(event).origin?.s3?.customHeaders[header][0].value;
 
-module.exports = ({ allowedMethods, event }) => new Promise(
-  (resolve, reject) => allowedMethods.indexOf(getMethod(event)) === -1 ?
-    reject({
+module.exports = ({
+  allowedMethods, bodyIncluded, event
+}) => new Promise((resolve, reject) => {
+  const method = getMethod(event);
+  const allowMethodsStr = allowedMethods.map(method => `"${method}"`).join(', ');
+  const methodsPlural = `method${allowedMethods.length === 1 ? '' : 's'}`;
+  const request = getRequest(event);
+  const body = request.body;
+
+  if (allowedMethods.indexOf(method) === -1) {
+    return reject({
       statusCode: 405,
-      error: `Only ${allowedMethods.map(method => `"${method}"`).join(', ')} method${allowedMethods.length === 1 ? '' : 's'} allowed`
-    }) :
-    resolve({
-      request: getRequest(event),
-      query: getQuery(event),
-      getCustomHeader: getCustomHeader(event)
-    })
-);
+      error: ['Only', allowMethodsStr, methodsPlural, 'allowed'].join(' ')
+    });
+  }
+
+  if (['HEAD', 'OPTIONS'].indexOf(method) !== -1) {
+    return reject({
+      statusCode: 200,
+      includeBody: false
+    });
+  }
+
+  if (bodyIncluded && (!body || !body.data)) {
+    return reject({
+      statusCode: 400,
+      error: ['No', method, 'body', 'sent'].join(' ')
+    });
+  }
+
+  return resolve({
+    request, query: getQuery(event),
+    body: bodyIncluded && Buffer.from(body.data, 'base64'),
+    getCustomHeader: getCustomHeader(event)
+  });
+});
